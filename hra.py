177c0c6a08d22fd9de32b36db69cfbe78ff89d4b
tomashonsa <tomasekhonsa@email.cz>
2024-05-14 13:44:18 +0200

    2
    2

--- hra.py
+++ hra.py
@@ -2,15 +2,16 @@ import random
 p = []
 d = random.randint(0,5)
 for x in range(d):
-        p.append(random.randint(0,10))
+    p.append(random.randint(0,10))
+print(p)
 y = len(p)
 print(y)
 a = input("jaká je délka pole?")
 a = int(a)
-if a==y:
-        print("správně")
 
+if a==y:
+      print("správně")
 else:
-        print("špatně")
-print(a)
-print(y)
+      print("špatně")
+
+
